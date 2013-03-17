ce7368ffb4f112b9476a39c046f7372540b74f20
Nelson Ray <ncray86@gmail.com>
2013-03-16 20:23:45 -0700

    first starting to move things to an appendix

---
+++ auxiliary-app/auto/auxiliary-app.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "auxiliary-app"
+ (lambda ()
+    (LaTeX-add-labels
+     "A:auxiliary-app"
+     "T:c_r-inequality"
+     "C:sum_variance"
+     "L:antisymmetric"
+     "eq:expectation-w-f(w)"
+     "E:EW'W"
+     "L:stein-eq-sol-bound"
+     "L:stein-eq-sol"
+     "eq:stein-eq"
+     "eq:fz"
+     "L:stein-eq-sol-bound2")))
+
---
+++ steins-method-app/auto/steins-method-app.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "steins-method-app"
+ (lambda ()
+    (LaTeX-add-labels
+     "A:steins-method-app"
+     "E:condexp-diff-sq"
+     "E:condexp-diff-sq2"
+     "eq:triple-bound"
+     "E:J1")))
+
