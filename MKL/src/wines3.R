ba44b85ab075444ac36e5271be97515b6cb428fb
Nelson Ray <ncray86@gmail.com>
2013-04-17 18:56:19 -0700

    wine update

--- MKL/src/wines3.R
+++ MKL/src/wines3.R
@@ -107,9 +107,13 @@ trainMKL <- function(desc = NULL, subregion = NULL, price = NULL, ratings = NULL
   
   if(length(subregion) > 0) dump <- sg('add_kernel', 1, 'CUSTOM', subr, "FULL")
 
-  if(length(price) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
-  if(length(ratings) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
-  if(length(year) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
+  ## if(length(price) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
+  ## if(length(ratings) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
+  ## if(length(year) > 0) dump <- sg('add_kernel', 1, 'LINEAR', 'REAL', cache_size)
+
+  if(length(price) > 0) dump <- sg('add_kernel', 1, 'GAUSSIAN', 'REAL', cache_size, 1)
+  if(length(ratings) > 0) dump <- sg('add_kernel', 1, 'GAUSSIAN', 'REAL', cache_size, 1)
+  if(length(year) > 0) dump <- sg('add_kernel', 1, 'GAUSSIAN', 'REAL', cache_size, 1)
 
   if(length(title.v) > 0){
     for(i in title.v){
@@ -125,35 +129,73 @@ trainMKL <- function(desc = NULL, subregion = NULL, price = NULL, ratings = NULL
 
 winePower <- function(n, mkl_norm = 2, C = .01){
   dat <- getWineData(n, dat3)
-  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = FALSE)(desc = desc, subregion = subregion, price = price, ratings = ratings, year = year, title = title, l = l, desc.v = 3, title.v = 3, mkl_norm = mkl_norm, C = C))
-  wts <- getMKLWeights()
-  names(wts) <- c("desc.K3", "subr.C", "price.L", "ratings.L", "year.L", "title.K3")
-  c("reject" = rej, "n" = n, wts, group = "Desc./SubR./Price/Ratings/Year/Title")
+  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = desc, subregion = subregion, price = price, ratings = ratings, year = year, title = title, l = l, desc.v = 4, title.v = 4, mkl_norm = mkl_norm, C = C))
+  wts <- data.frame(getMKLWeights())
+  names(wts) <- c("desc.K4", "subr.C", "price.R1", "ratings.R1", "year.R1", "title.K4")
+  wts$reject <- rej
+  wts$n <- n
+  wts$C <- C
+  wts$group <- "Desc./SubR./Price/Ratings/Year/Title"
+  wts
 }
 
-system.time(res1 <- ldply(rep(c(seq(50, 350, 50)), each = 50), winePower, .parallel = TRUE))
+system.time(res1 <- ldply(rep(c(seq(50, 200, 50)), each = 100), winePower, .parallel = TRUE))
+
+##qplot(x = variable, y = value, geom = "boxplot", data = melt(res1[, ], id.vars = c("n", "group")))
+
+## system.time(res1 <- mdply(expand.grid(n = rep(c(seq(50, 200, 50)), each = 100),
+##                                       C = c(.001, .01, .1, 1)), winePower, .parallel = TRUE))
+## res.b <- ddply(res1, .(n, group, C), function(df){
+##   x <- df$reject
+##   lims <- c(.025, .975)
+##   bootM <- function(x) quantile(as.vector(boot(x, function(x, i) mean(x[i]), 1000)$t), lims)
+##   boot <- bootM(x)
+##   data.frame("value" = mean(x), "lower" = boot[1], "upper" = boot[2], "group" = df$group[1])
+##   })
+## qplot(x = n, y = value, data = res.b, geom = "line", color = factor(C)) +
+##   geom_errorbar(aes(ymin = lower, ymax = upper, width = 7)) 
 
 winePower <- function(n, mkl_norm = 2, C = .01){
   dat <- getWineData(n, dat3)
-  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = FALSE)(desc = desc, subregion = subregion, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = 3, title.v = NULL, mkl_norm = mkl_norm, C = C))
-  wts <- getMKLWeights()
-  names(wts) <- c("desc.K3", "subr.C", "price.L", "ratings.L", "year.L")
-  c("reject" = rej, "n" = n, wts, group = "Desc./SubR./Price/Ratings/Year")
+  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = desc, subregion = subregion, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = 4, title.v = NULL, mkl_norm = mkl_norm, C = C))
+  wts <- data.frame(getMKLWeights())
+  names(wts) <- c("desc.K3", "subr.C", "price.R1", "ratings.R1", "year.R1")
+  wts$reject <- rej
+  wts$n <- n
+  wts$C <- C
+  wts$group <- "Desc./SubR./Price/Ratings/Year"
+  wts
 }
 
-system.time(res2 <- ldply(rep(c(seq(50, 350, 50)), each = 50), winePower, .parallel = TRUE))
+system.time(res2 <- ldply(rep(c(seq(50, 200, 50)), each = 100), winePower, .parallel = TRUE))
+##qplot(x = variable, y = value, geom = "boxplot", data = melt(res2[, ], id.vars = c("n", "group")))
 
-winePower <- function(n, mkl_norm = 2, C = 3){
-  print(n)
+## winePower <- function(n, mkl_norm = 2, C = .01){
+##   dat <- getWineData(n, dat3)
+##   rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = desc, subregion = NULL, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = 2, title.v = NULL, mkl_norm = mkl_norm, C = C))
+##   wts <- data.frame(getMKLWeights())
+##   names(wts) <- c("desc.K3", "price.L", "ratings.L", "year.L")
+##   wts$reject <- rej
+##   wts$n <- n
+##   wts$C <- C
+##   wts$group <- "Desc./Price/Ratings/Year"
+##   wts
+## }
+
+winePower <- function(n, mkl_norm = 2, C = .01){
   dat <- getWineData(n, dat3)
-  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = FALSE)(desc = desc, subregion = NULL, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = 3, title.v = NULL, mkl_norm = mkl_norm, C = C))
-  wts <- getMKLWeights()
-  names(wts) <- c("desc.K3", "price.L", "ratings.L", "year.L")
-  c("reject" = rej, "n" = n, wts, group = "Desc./Price/Ratings/Year", "C" = C)
+  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = NULL, subregion = subregion, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = NULL, title.v = NULL, mkl_norm = mkl_norm, C = C))
+  wts <- data.frame(getMKLWeights())
+  names(wts) <- c("subr.C", "price.R1", "ratings.R1", "year.R1")
+  wts$reject <- rej
+  wts$n <- n
+  wts$C <- C
+  wts$group <- "SubR./Price/Ratings/Year"
+  wts
 }
 
 #system.time(res3.2 <- ldply(rep(c(.5, 1, 5, 10, 100), each = 5), function(C) winePower(500, 2, C), .parallel = TRUE))
-#system.time(res3.2 <- ldply(rep(as.numeric(4:8), each = 50), function(C) winePower(350, 2, C), .parallel = TRUE))
+#system.time(res3.2 <- ldply(rep(as.numeric(1:8), each = 100), function(C) winePower(350, 2, C), .parallel = TRUE))
 ##     n C   V1
 ## 1 350 1 0.08
 ## 2 350 2 0.10
@@ -163,29 +205,97 @@ winePower <- function(n, mkl_norm = 2, C = 3){
 ## 3 350 6 0.18
 ## 4 350 7 0.20
 ## 5 350 8 0.12
-#ddply(res3.2, .(n, C), function(df) mean(as.logical(df$reject.t)))
 
-system.time(res3 <- ldply(rep(c(seq(50, 350, 50)), each = 50), winePower, .parallel = TRUE))
+##new
+##     n C   V1
+## 1 350 1 0.04
+## 2 350 2 0.14
+## 3 350 3 0.34
+## 4 350 4 0.28
+## 5 350 5 0.34
+## 6 350 6 0.28
+## 7 350 7 0.36
+## 8 350 8 0.44
+#ddply(res3.2, .(n, C), function(df) mean(as.logical(df$reject)))
 
-res <- rbind(res1[, c("reject.t", "n", "group")], res2[, c("reject.t", "n", "group")], res3[, c("reject.t", "n", "group")])
-res$reject.t <- as.logical(res$reject.t)
-res$n <- as.numeric(res$n)
-qplot(x = n, y = V1, data = ddply(res, .(n, group), function(df) mean(df$reject.t)), geom = "line", color = group)
-qplot(x = n, y = V1, data = subset(ddply(res, .(n, group), function(df) mean(df$reject.t)), n < 350), geom = "line", color = group)
-ggsave("wine_power.png")
+system.time(res3 <- ldply(rep(c(seq(50, 200, 50)), each = 100), winePower, .parallel = TRUE))
+
+##qplot(x = variable, y = value, geom = "boxplot", data = melt(res3[, ], id.vars = c("n", "group")))
+
+##res.m <- melt(res3[, ], id.vars = c("n", "group"))
+##qplot(x = variable, y = value, geom = "boxplot", data = res.m)
 
 winePower <- function(n, mkl_norm = 2, C = .01){
+  print(n)
   dat <- getWineData(n, dat3)
-  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = FALSE)(desc = NULL, subregion = NULL, price = price, ratings = ratings, year = NULL, title = NULL, l = l, desc.v = NULL, title.v = NULL, mkl_norm = mkl_norm, C = C))
-  wts <- getMKLWeights()
-  names(wts) <- c("price.L", "ratings.L")
-  c("reject" = rej, "n" = n, wts)
+  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = NULL, subregion = NULL, price = price, ratings = ratings, year = year, title = NULL, l = l, desc.v = NULL, title.v = NULL, mkl_norm = mkl_norm, C = C))
+  wts <- data.frame(getMKLWeights())
+  names(wts) <- c("price.R1", "ratings.R1", "year.R1")
+  wts$reject <- rej
+  wts$n <- n
+  wts$C <- C
+  wts$group <- "Price/Ratings/Year"
+  wts
 }
 
-system.time(res <- ldply(rep(c(seq(50, 250, 50)), each = 20), winePower, .parallel = TRUE))
+system.time(res4 <- ldply(rep(c(seq(50, 200, 50)), each = 100), winePower, .parallel = TRUE))
+##qplot(x = variable, y = value, geom = "boxplot", data = melt(res4[, ], id.vars = c("n", "group")))
+## system.time(res4.2 <- mdply(expand.grid(n = rep(c(seq(50, 200, 50)), each = 50),
+##                                         C = c(.001, .01, .1, 1, 3)), winePower, .parallel = TRUE))
+
+
+winePower <- function(n, mkl_norm = 2, C = .01){
+  print(n)
+  dat <- getWineData(n, dat3)
+  rej <- with(dat, reject(compute(trainMKL), parametric = TRUE, verbose = TRUE)(desc = NULL, subregion = NULL, price = price, ratings = NULL, year = year, title = NULL, l = l, desc.v = NULL, title.v = NULL, mkl_norm = mkl_norm, C = C))
+  wts <- data.frame(getMKLWeights())
+  names(wts) <- c("price.R1", "year.R1")
+  wts$reject <- rej
+  wts$n <- n
+  wts$C <- C
+  wts$group <- "Price/Year"
+  wts
+}
+
+system.time(res5 <- ldply(rep(c(seq(50, 200, 50)), each = 100), winePower, .parallel = TRUE))
+##qplot(x = variable, y = value, geom = "boxplot", data = melt(res5[, ], id.vars = c("n", "group")))
+
+res <- rbind(res1[, c("reject", "n", "group")],
+             res2[, c("reject", "n", "group")],
+             res3[, c("reject", "n", "group")],
+             res4[, c("reject", "n", "group")],
+             res5[, c("reject", "n", "group")])
+
+res.b <- ddply(res, .(n, group), function(df){
+  x <- df$reject
+  lims <- c(.025, .975)
+  bootM <- function(x) quantile(as.vector(boot(x, function(x, i) mean(x[i]), 1000)$t), lims)
+  boot <- bootM(x)
+  data.frame("value" = mean(x), "lower" = boot[1], "upper" = boot[2], "group" = df$group[1])
+  })
+
+p1 <- ggplot(res.b, aes(x = n, y = value, color = group, linetype = group)) +
+  geom_line() + 
+  geom_errorbar(aes(ymin = lower, ymax = upper, width = 7))
+p1
+  ##facet_grid(p~.) +
+  ##ggtitle("Power (Christmas Star Example)") +
+  ##xlab("Radius of Outer Star (Inner is 4)") +
+  ##ylab("Power")
+
+qplot(x = n, y = V1, data = ddply(res, .(n, group), function(df) mean(df$reject.t)), geom = "line", color = group)
+##qplot(x = n, y = V1, data = subset(ddply(res, .(n, group), function(df) mean(df$reject.t)), n < 350), geom = "line", color = group)
+##ggsave("wine_power.png")
+
+##system.time(res3.2 <- ldply(rep(as.numeric(3), each = 200), function(C) winePower(350, 2, C), .parallel = TRUE))
+##mean(as.logical(res3.2$reject.t))
+##[1] 0.21
+
+
 qplot(x = n, y = V1, data = ddply(res, .(n), function(df) mean(df$reject.t)), geom = "line")
-res.m <- melt(res[, -1], id.vars = "n")
+res.m <- melt(res3[, ], id.vars = c("n", "group"))
 qplot(x = variable, y = value, geom = "boxplot", data = res.m)
+
 
 
 
